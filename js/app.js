a6f92c4510f622d234b8347b6b275203643d047c
georgealv96 <george1996alvarado@gmail.com>
2023-06-08 14:32:33 -0600

    lab completed

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// Menu data structure
+const menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' }
+]
+
+const mainEl = document.querySelector('main')
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+
+mainEl.classList.add('flex-ctr')
+
+topMenuEl = document.getElementById('top-menu')
+
+topMenuEl.style.height = '100%'
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+
+topMenuEl.classList.add('flex-around')
+
+menuLinks.forEach(function (menuLink, i) {
+  anchor = document.createElement('a')
+  anchor.setAttribute('href', menuLinks[i].href)
+  anchor.innerText = menuLinks[i].text
+  topMenuEl.appendChild(anchor)
+})
